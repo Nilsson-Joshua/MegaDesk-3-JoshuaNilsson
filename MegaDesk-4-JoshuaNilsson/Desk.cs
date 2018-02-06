170381ae21a762265c48abc740540347c0d51c06
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-02-05 19:11:39 -0700

    initial commit

--- MegaDesk-4-JoshuaNilsson/Desk.cs
+++ MegaDesk-4-JoshuaNilsson/Desk.cs
@@ -9,7 +9,9 @@ namespace MegaDesk_3_JoshuaNilsson
         public int NumDrawers { get; set; }
         public string MaterialType { get; set; }
 
+        // constants
         public const int MINWIDTH = 24;
+        public const int MIDWIDTH = 25;
         public const int MAXWIDTH = 96;
         public const int MINDEPTH = 12;
         public const int MAXDEPTH = 48;
